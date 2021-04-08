c4c1ef2c32608f729595cb7b3e01541412cabc9d
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2021-04-08 15:00:05 +0800

    v1.1 add function of sending broadcast but it won't work......

--- app/src/main/java/com/example/broadcast/MainActivity.java
+++ app/src/main/java/com/example/broadcast/MainActivity.java
@@ -9,6 +9,8 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import java.sql.Connection;
@@ -16,6 +18,9 @@ import java.sql.Connection;
 public class MainActivity extends AppCompatActivity {
     private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
+    //private BootComplete bootComplete;
+    //private MyBroadcastReceiver myBroadcastReceiver;
+    private Button broadcast_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +29,18 @@ public class MainActivity extends AppCompatActivity {
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
+        //myBroadcastReceiver = new MyBroadcastReceiver();
+        //bootComplete = new BootComplete();
         registerReceiver(networkChangeReceiver,intentFilter);
+
+        broadcast_bt = (Button)findViewById(R.id.broadcast_bt);
+        broadcast_bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent broadcastIntent = new Intent("com.example.broadcasttest.MY_MESSAGE");
+                sendBroadcast(broadcastIntent);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/broadcast/MyBroadcastReceiver.java
@@ -0,0 +1,15 @@
+package com.example.broadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context,"message received!",Toast.LENGTH_SHORT).show();
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
