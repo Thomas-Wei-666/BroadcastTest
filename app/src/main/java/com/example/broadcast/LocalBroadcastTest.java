fa0764188a64273a2f6d9e0c47dcba0d4b00c9cb
Thomas-Wei-666 <72587206+Thomas-Wei-666@users.noreply.github.com>
2021-04-12 20:28:06 +0800

    v1.3.1 修改了代码，使其更加规范。 Java文件中变量全部使用驼峰命名;OnClickListener处替换为Lamda表达式（简化括号）; 调整了格式（ctrl+alt+l）

--- app/src/main/java/com/example/broadcast/LocalBroadcastTest.java
+++ app/src/main/java/com/example/broadcast/LocalBroadcastTest.java
@@ -17,11 +17,10 @@ import android.widget.Toast;
 public class LocalBroadcastTest extends AppCompatActivity {
     private LocalBroadcastReceiver localBroadcastReceiver;
     private LocalBroadcastManager localBroadcastManager;
-    private Button send_localbroadcast_bt;
     private final String ACTION = "com.example.broadcast.LOCAL_MESSAGE";
 
-    public static void startLocalBroadcastTest(Activity activity){
-        Intent intent = new Intent(activity,LocalBroadcastTest.class);
+    public static void startLocalBroadcastTest(Activity activity) {
+        Intent intent = new Intent(activity, LocalBroadcastTest.class);
         activity.startActivity(intent);
     }
 
@@ -42,24 +41,22 @@ public class LocalBroadcastTest extends AppCompatActivity {
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION);
 
-        localBroadcastManager.registerReceiver(localBroadcastReceiver,filter);
+        localBroadcastManager.registerReceiver(localBroadcastReceiver, filter);
 
-        send_localbroadcast_bt = (Button)findViewById(R.id.send_localbroadcast_bt);
-        send_localbroadcast_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent_message = new Intent(ACTION);
-                //intent_message.setComponent(new ComponentName(getPackageName(),getPackageName()+"."))
-                // 动态广播接收器不需要在发送广播之前指定接收器位置！
-                localBroadcastManager.sendBroadcast(intent_message);
-            }
+        Button sendLocalBroadcastBt = (Button) findViewById(R.id.send_localbroadcast_bt);
+        sendLocalBroadcastBt.setOnClickListener(v -> {
+            Intent intent_message = new Intent(ACTION);
+            //intent_message.setComponent(new ComponentName(getPackageName(),getPackageName()+"."))
+            // 动态广播接收器不需要在发送广播之前指定接收器位置！
+            localBroadcastManager.sendBroadcast(intent_message);
         });
 
     }
 }
-class LocalBroadcastReceiver extends BroadcastReceiver{
+
+class LocalBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context,"localbroadcast received",Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, "localbroadcast received", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/broadcast/MainActivity.java
+++ app/src/main/java/com/example/broadcast/MainActivity.java
@@ -17,37 +17,26 @@ import android.widget.Toast;
 import java.sql.Connection;
 
 public class MainActivity extends AppCompatActivity {
-    private IntentFilter intentFilter;
     private NetworkChangeReceiver networkChangeReceiver;
-    private Button broadcast_bt;
-    private Button start_act2_bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
+        IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver,intentFilter);
 
-        broadcast_bt = (Button)findViewById(R.id.broadcast_bt);
-        start_act2_bt = (Button)findViewById(R.id.start_act2);
-        broadcast_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent broadcastIntent = new Intent(Intent.ACTION_EDIT);
-                broadcastIntent.setComponent(new ComponentName(getPackageName(),getPackageName()+".MyBroadcastReceiver"));
-                //Android8.0以上版本发送广播的时候需要加上setComponent....，指定接收器的包名和完整路径名....
-                MainActivity.this.sendBroadcast(broadcastIntent);
-            }
+        Button broadcastBt = (Button)findViewById(R.id.broadcast_bt);
+        Button startAct2Bt = (Button)findViewById(R.id.start_act2);
+        broadcastBt.setOnClickListener(v -> {
+            Intent broadcastIntent = new Intent(Intent.ACTION_EDIT);
+            broadcastIntent.setComponent(new ComponentName(getPackageName(), getPackageName() + ".MyBroadcastReceiver"));
+            //Android8.0以上版本发送广播的时候需要加上setComponent....，指定接收器的包名和完整路径名....
+            MainActivity.this.sendBroadcast(broadcastIntent);
         });
-        start_act2_bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                LocalBroadcastTest.startLocalBroadcastTest(MainActivity.this);
-            }
-        });
+        startAct2Bt.setOnClickListener(v -> LocalBroadcastTest.startLocalBroadcastTest(MainActivity.this));
     }
 
     @Override
@@ -56,16 +45,15 @@ public class MainActivity extends AppCompatActivity {
         unregisterReceiver(networkChangeReceiver);
     }
 
-    class NetworkChangeReceiver extends BroadcastReceiver{
+    class NetworkChangeReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
-            ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
             NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
-            if(networkInfo != null&&networkInfo.isAvailable()){
-                Toast.makeText(context,"network is now available",Toast.LENGTH_SHORT).show();
-            }
-            else {
-                Toast.makeText(context,"network is now broken",Toast.LENGTH_SHORT).show();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is now available", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, "network is now broken", Toast.LENGTH_SHORT).show();
             }
         }
     }
